57896356f6f47a1de097af041fe29feb4c34b3b6
v689777 <thomas.kurien@jpmorgan.com>
2019-06-27 14:20:38 -0500

    added new bitmap resizing method to maintain scale ration

--- FraudDetector/app/src/main/java/com/fraudulentteam/frauddetector/ImageViewActivity.java
+++ FraudDetector/app/src/main/java/com/fraudulentteam/frauddetector/ImageViewActivity.java
@@ -70,6 +70,25 @@ public class ImageViewActivity extends AppCompatActivity {
         });
 
     }
+    public Bitmap resizeBitmap(String photoPath, int targetW, int targetH) {
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+       // BitmapFactory.
+        BitmapFactory.decodeFile(photoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true; //Deprecated API 21
+
+        return BitmapFactory.decodeFile(photoPath, bmOptions);
+    }
 
     private void init() {
         try {
